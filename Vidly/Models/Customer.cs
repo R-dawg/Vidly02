3200d91b205d5fb9cc40dc626f5f4d17f5a1d020
Randall <ragrate@gmail.com>
2017-06-05 21:39:12 -0400

    Set up the database to hold customer name, membership type, and discount.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -15,5 +15,6 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }  // Navigation property
         public byte MembershipTypeId { get; set; }
+        public DateTime Birthdate { get; set; }
     }
 }
